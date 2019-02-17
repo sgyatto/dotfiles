2476e0ab2207b5da4a8f2f30362ad53e7830aa20
sgyatto <42696711+sgyatto@users.noreply.github.com>
2019-02-18 02:18:50 +0900

    Update .tmux.conf

--- link.sh
+++ link.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
