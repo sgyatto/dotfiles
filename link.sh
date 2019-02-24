febe49d1a29db705171f5d216cb41c0f0d1b59cc
sgyatto <42696711+sgyatto@users.noreply.github.com>
2019-02-25 01:18:20 +0900

    Delete .zshenv

--- link.sh
+++ link.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
